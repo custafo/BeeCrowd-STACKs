02a5de7448bcfc30404395dd9cdb22081894561a
GUSTAVO PEREIRA OLIVEIRA LIMA <gustavo.00000844518@unicap.br>
2023-03-27 15:32:16 +0000

    STACK DONE

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int times = in.nextInt();
+        in.nextLine();
+
+        for (int index = 0; index < times; index++) {
+            Stack<String> openDiamonds = new Stack<String>();
+            int countDiamonds = 0;
+            String mine = in.nextLine();
+            String[] arrayItemsInMine = mine.split("");
+        
+            for (String currentChar : arrayItemsInMine) {
+                if(currentChar.equals("<")){
+                    openDiamonds.push(currentChar);
+                }
+                else{
+                    if(currentChar.equals(">") && !openDiamonds.empty()){
+                        openDiamonds.pop();
+                        countDiamonds++;
+                    }
+                }
+            }
+
+            System.out.println(countDiamonds);
+        }
+    }
+}
